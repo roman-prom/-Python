4a6f1f44a8e0c14c5374cbc933170d2183344c28
roman-prom <124841859+roman-prom@users.noreply.github.com>
2023-10-16 01:42:08 +0300

    main.py

--- main.py
+++ main.py
@@ -16,35 +16,35 @@ class Main(tk.Frame):
         toolbar.pack(side=tk.TOP, fill=tk.X)
         
         # создание кнопки добавления контакта
-        self.add_img = tk.PhotoImage(file='./Module 4/ввыы/img/add.png')
+        self.add_img = tk.PhotoImage(file='./img/add.png')
         btn_add = tk.Button(toolbar, bg='#d7d7d7', bd=0,
                             image=self.add_img,
                             command=self.open_dialog)
         btn_add.pack(side=tk.LEFT)
         
         # создание кнопки редактирования контакта
-        self.edit_img = tk.PhotoImage(file='./Module 4/ввыы/img/update.png')
+        self.edit_img = tk.PhotoImage(file='./img/update.png')
         btn_edit = tk.Button(toolbar, bg='#d7d7d7', bd=0,
                             image=self.edit_img,
                             command=self.open_edit)
         btn_edit.pack(side=tk.LEFT)
         
         # создание кнопки удаления контакта
-        self.del_img = tk.PhotoImage(file='./Module 4/ввыы/img/delete.png')
+        self.del_img = tk.PhotoImage(file='./img/delete.png')
         btn_del = tk.Button(toolbar, bg='#d7d7d7', bd=0,
                             image=self.del_img,
                             command=self.delete_records)
         btn_del.pack(side=tk.LEFT)
         
         # создание кнопки поиска контакта
-        self.search_img = tk.PhotoImage(file='./Module 4/ввыы/img/search.png')
+        self.search_img = tk.PhotoImage(file='./img/search.png')
         btn_search = tk.Button(toolbar, bg='#d7d7d7', bd=0,
                             image=self.search_img,
                             command=self.open_search)
         btn_search.pack(side=tk.LEFT)
         
         # создание кнопки обновления таблицы
-        self.refresh_img = tk.PhotoImage(file='./Module 4/ввыы/img/refresh.png')
+        self.refresh_img = tk.PhotoImage(file='./img/refresh.png')
         btn_refresh = tk.Button(toolbar, bg='#d7d7d7', bd=0,
                             image=self.refresh_img,
                             command=self.view_records)
@@ -52,7 +52,7 @@ class Main(tk.Frame):
         
         # создание таблицы
         self.tree = ttk.Treeview(root, 
-                                 columns=('id', 'name', 'tel', 'email'),
+                                 columns=('id', 'name', 'tel', 'email', 'salary'),
                                  height=45,
                                  show='headings')
         # добавляем параметры столбцам
@@ -60,11 +60,13 @@ class Main(tk.Frame):
         self.tree.column('name', width=300, anchor=tk.CENTER)
         self.tree.column('tel', width=150, anchor=tk.CENTER)
         self.tree.column('email', width=150, anchor=tk.CENTER)
+        self.tree.column('salary', width=150, anchor=tk.CENTER)
         
         self.tree.heading('id', text='id')
         self.tree.heading('name', text='ФИО')
         self.tree.heading('tel', text='Номер телефона')
         self.tree.heading('email', text='Эл.почта')
+        self.tree.heading('salary', text='Зарплата')
         self.tree.pack(side=tk.LEFT)
         
         # полоса прокрутки
@@ -73,17 +75,17 @@ class Main(tk.Frame):
         self.tree.configure(yscrollcommand=scroll.set)
         
     # метод добавления (посредник)
-    def records(self, name, tel, email):
-        self.db.insert_data(name, tel, email)
+    def records(self, name, tel, email, salary):
+        self.db.insert_data(name, tel, email, salary)
         self.view_records()
         
     # метод редактирования
-    def edit_record(self, name, tel, email):
+    def edit_record(self, name, tel, email, salary):
         ind = self.tree.set(self.tree.selection()[0], '#1')
         self.db.cur.execute('''
-            UPDATE сотрудники SET name = ?, phone = ?, email = ?
+            UPDATE сотрудники SET name = ?, phone = ?, email = ?, email = ?
             WHERE id = ?
-        ''', (name, tel, email, ind))
+        ''', (name, tel, email, salary, ind))
         self.db.conn.commit()
         self.view_records()
         
@@ -134,7 +136,7 @@ class Child(tk.Toplevel):
         
     def init_child(self):
         self.title('Добавление нового сотрудника')
-        self.geometry('400x200')
+        self.geometry('400x170')
         # запрет на изменение размеров
         self.resizable(False, False)
         # перезватаваеи все события приложения
@@ -144,30 +146,35 @@ class Child(tk.Toplevel):
         
         # создание формы
         label_name = tk.Label(self, text='ФИО')
-        label_name.place(x=50, y=50)
+        label_name.place(x=50, y=25)
         label_tel = tk.Label(self, text='Телефон')
-        label_tel.place(x=50, y=75)
+        label_tel.place(x=50, y=50)
         label_email = tk.Label(self, text='Эл.почта')
-        label_email.place(x=50, y=100)
+        label_email.place(x=50, y=75)
+        label_salary = tk.Label(self, text='Зарплата')
+        label_salary.place(x=50, y=100)
         
         self.entry_name = tk.Entry(self)
-        self.entry_name.place(x=150, y=50)
+        self.entry_name.place(x=150, y=25)
         self.entry_tel = tk.Entry(self)
-        self.entry_tel.place(x=150, y=75)
+        self.entry_tel.place(x=150, y=50)
         self.entry_email = tk.Entry(self)
-        self.entry_email.place(x=150, y=100)
+        self.entry_email.place(x=150, y=75)
+        self.entry_salary = tk.Entry(self)
+        self.entry_salary.place(x=150, y=100)
         
         self.btn_ok = tk.Button(self, text='Добавить')
         self.btn_ok.bind('<Button-1>', lambda ev: 
                     self.view.records(
                         self.entry_name.get(),
                         self.entry_tel.get(),
-                        self.entry_email.get()
+                        self.entry_email.get(),
+                        self.entry_salary.get()
                         ))
-        self.btn_ok.place(x=225, y=160)
+        self.btn_ok.place(x=225, y=130)
         
         btn_cancel = tk.Button(self, text='Закрыть', command=self.destroy)
-        btn_cancel.place(x=300, y=160)
+        btn_cancel.place(x=300, y=130)
         
 class Update(Child):
     def __init__(self):
@@ -187,7 +194,8 @@ class Update(Child):
                     self.view.edit_record(
                         self.entry_name.get(),
                         self.entry_tel.get(),
-                        self.entry_email.get()
+                        self.entry_email.get(),
+                        self.entry_salary.get()
                         ))
         self.btn_ok.bind('<Button-1>', lambda ev: self.destroy(), add='+')
         self.btn_ok.place(x=195, y=160)
@@ -200,6 +208,7 @@ class Update(Child):
         self.entry_name.insert(0, row[1])
         self.entry_tel.insert(0, row[2])
         self.entry_email.insert(0, row[3])
+        self.entry_salary.insert(0, row[3])
 
 # класс окна поиска
 class Search(tk.Toplevel):
@@ -247,14 +256,15 @@ class Db:
                     id INTEGER PRIMARY KEY,
                     name TEXT,
                     phone TEXT,
-                    email TEXT
+                    email TEXT,
+                    salary TEXT
                 )''')
         
     # метод добавления в бд
-    def insert_data(self, name, tel, email):
+    def insert_data(self, name, tel, email, salary):
         self.cur.execute('''
-                INSERT INTO сотрудники (name, phone, email)
-                VALUES (?, ?, ?)''', (name, tel, email))
+                INSERT INTO сотрудники (name, phone, email, salary)
+                VALUES (?, ?, ?, ?)''', (name, tel, email, salary))
         self.conn.commit()
         
         
@@ -265,7 +275,7 @@ if __name__ == '__main__':
     db = Db()
     app = Main(root)
     root.title('Список сотрудников компании')
-    root.geometry('665x400')
+    root.geometry('800x400')
     # запрет на изменение размеров
     root.resizable(False, False)
     root.mainloop()
